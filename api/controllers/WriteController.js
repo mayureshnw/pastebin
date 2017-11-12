b04e44bd679728b65b574918872dcc22952d6e19
Mayuresh Waykole <mayuresh2212@gmail.com>
2017-11-12 14:16:29 -0500

    WIP

--- api/controllers/WriteController.js
+++ api/controllers/WriteController.js
@@ -1,3 +1,12 @@
+const uploadOptions = {
+	key: process.env.AWS_KEY,
+	secret: process.env.AWS_SECRET,
+	bucket: process.env.BUCKET,
+}
+const adapter = require('skipper-better-s3')(options)
+const receiver = adapter.receive("./s3/tmp/")
+const Readable = require("stream").Readable
+
 const write = (req, res) => {
 	const text = req.param('text');
 	const exp = req.param('expiraion')
@@ -21,7 +30,15 @@ const write = (req, res) => {
 }
 
 const updateToS3 = (text) => {
-	return Promise.resolve('/123/456')
+	s = new Readable()
+	s.push(text)
+	s.push(null)
+	receiver.write(file, () => {
+		// Upload complete! You can find some interesting info on the stream's
+		// `extra` property
+		console.log(file.extra)
+		res.send("")
+	})
 }
 
 const isValid = (text) => (typeof text === 'undefined') || (text === "")
